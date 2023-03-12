0d774737b7e126369987f85ba522104761887ff8
marina-hoffmann <marina.hoffmann@aurum.com.br>
2023-03-12 08:27:21 -0300

    cria footer

--- cardapio/client/src/App.jsx
+++ cardapio/client/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import { Navbar } from './components/navbar/Navbar'
+import { Footer } from './components/footer/Footer'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
+      <main className='main'>
+        <h2>Produtos</h2>
+      </main>
+      <Footer />
     </div>
   )
 }
